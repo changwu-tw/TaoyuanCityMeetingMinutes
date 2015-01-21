e85168426fc4da1aa90f8bd4754c6c64c5dd19a6
changwu <changwu.me@gmail.com>
2015-01-22 06:43:17 +0800

    initial

---
+++ MeetingMinutes.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import datetime
+import filecmp
+import os
+import pytz
+import requests
+import sys
+import urllib
+
+
+from bs4 import BeautifulSoup
+
+reload(sys)
+sys.setdefaultencoding("utf-8")
+
+BASE = 'http://www.tycg.gov.tw/ch/'
+TODAY = datetime.datetime.now(pytz.timezone('US/Central')).strftime('%Y%m%d')
+
+
+def isSameFile(file1, file2):
+    return filecmp.cmp(file1, file2)
+
+
+def extarctPdf(path):
+    from cStringIO import StringIO
+    from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+    from pdfminer.converter import TextConverter
+    from pdfminer.layout import LAParams
+    from pdfminer.pdfpage import PDFPage
+
+    output = StringIO()
+    manager = PDFResourceManager()
+    converter = TextConverter(manager, output, laparams=LAParams())
+    interpreter = PDFPageInterpreter(manager, converter)
+
+    infile = file(path, 'rb')
+    for page in PDFPage.get_pages(infile):
+        interpreter.process_page(page)
+    infile.close()
+    converter.close()
+    text = output.getvalue()
+    output.close
+    return text
+
+
+def getPdfUrl(url):
+    pdfList = []
+    r = requests.get(url)
+    bs = BeautifulSoup(r.text).findAll('td', {'class', 'style_44-3'})
+    for td in bs:
+        path = td.find('a')['href']
+        filename = td.find('a').text
+        pdfList.append((path, filename))
+    return pdfList
+
+
+def getMeetingList():
+    r = requests.get('http://www.tycg.gov.tw/ch/home.jsp?id=10233&parentpath=0,4')
+    bs = BeautifulSoup(r.text).findAll('a', {'class', 'news_a'}, href=True)
+    return [BASE+a['href'] for a in bs]
+
+
+if __name__ == '__main__':
+    urlList = getMeetingList()
+
+    for url in urlList:
+        pdfList = getPdfUrl(url)
+        for (path, filename) in pdfList:
+            if os.path.isfile(filename):
+                # new file
+                source = urllib.urlopen(path).read()
+                newname = filename[:filename.find('.')] + TODAY + '.pdf'
+                with open(newname, 'w') as f:
+                    f.write(source)
+                # compare two files
+                if isSameFile(filename, newname):
+                    os.remove(newname)
+                else:
+                    textname = newname[:newname.find('.')] + '.txt'
+                    with open(textname, 'w') as f:
+                        f.write(extarctPdf(newname))
+            else:
+                # pdf
+                source = urllib.urlopen(path).read()
+                with open(filename, 'w') as f:
+                    f.write(source)
+                # txt
+                textname = filename[:filename.find('.')] + '.txt'
+                with open(textname, 'w') as f:
+                    f.write(extarctPdf(filename))
+
